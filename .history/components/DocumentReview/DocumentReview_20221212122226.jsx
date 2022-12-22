cf05a479af4aef8d569960ca449e1d3123e695f0
erenkahraman <110972375+erenkahraman@users.noreply.github.com>
2022-12-22 23:01:02 +0300

    default current day

---
+++ .history/components/DocumentReview/DocumentReview_20221212122226.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const DocumentReview = ({ title, register, type }) => {
+
+  const options = [
+    { value: "Not Submitted", label: "Not Submitted" },
+    { value: "Needs Review", label: "Needs Review" },
+    { value: "Incorrect", label: "Incorrect" },
+    { value: "Correct", label: "Correct" },
+  ];
+  
+
+  return (
+    <div className="flex w-48 flex-col gap-2">
+      <label htmlFor="status" className="block text-sm font-semibold">
+        {title}
+      </label>
+      <select
+        {...register(`${type}.documents.${title}`, {
+          required: `Please, submit the ${title}`,
+        })}
+        className="block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+      >
+        <option value="">Select</option>
+        <option value="Not Submitted">Not Submitted</option>
+        <option value="Needs Review">Needs Review</option>
+        <option value="Incorrect">Incorrect</option>
+        <option value="Correct">Correct</option>
+      </select>
+    </div>
+  );
+};
+
+export default DocumentReview;
